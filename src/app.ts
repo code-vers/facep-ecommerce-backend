57fcf97e78aa5e284cc84e7417adaeb7409e4139
FyslAhmd <info.faysal.32@gmail.com>
2026-07-23 16:01:58 +0600

    refactor: remove Swagger-related dependencies and documentation generation

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,12 @@ import cors, { type CorsOptions } from 'cors';
 import express, { type Application, type Request, type Response } from 'express';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
-import swaggerUi from 'swagger-ui-express';
 
 import config from './app/config';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
 import sendResponse from './app/utils/sendResponse';
-import { generateSwaggerDocs } from './app/utils/swagger';
 
 const app: Application = express();
 
@@ -28,9 +26,6 @@ app.use(cookieParser());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-// Setup Swagger Docs
-const swaggerDocs = generateSwaggerDocs();
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(
   '/api',
